ed79a5e46ef3f3b2d906af810786773ce9177905
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2025-10-18 15:41:52 -0400

    Auto update application identifier when changing the product or company name

---
+++ Assets/Editor/Scripts/AppIdManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+
+[InitializeOnLoad]
+public class AppIdManager
+{
+    private static string _previousCompany;
+    private static string _previousProduct;
+
+    static AppIdManager()
+    {
+        EditorApplication.update += Update;
+    }
+
+    private static void Update()
+    {
+        if (PlayerSettings.companyName == _previousCompany && PlayerSettings.productName == _previousProduct)
+            return;
+
+        UnifyAppIdentifier();
+        _previousCompany = PlayerSettings.companyName;
+        _previousProduct = PlayerSettings.productName;
+    }
+
+    private static void UnifyAppIdentifier()
+    {
+        string targetIdentifier = $"com.{PlayerSettings.companyName}.{PlayerSettings.productName}".ToLowerInvariant().Replace(" ", "");
+
+        PlayerSettings.SetApplicationIdentifier(NamedBuildTarget.iOS, targetIdentifier);
+        PlayerSettings.SetApplicationIdentifier(NamedBuildTarget.Android, targetIdentifier);
+        PlayerSettings.SetApplicationIdentifier(NamedBuildTarget.Standalone, targetIdentifier);
+    }
+}
