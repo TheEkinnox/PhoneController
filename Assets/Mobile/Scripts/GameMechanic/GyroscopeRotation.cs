7aab01a95417dc385e44cccbedb4aa1eab46e2d3
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2025-12-15 17:06:55 -0500

    Client gyro cleanup

--- Assets/Mobile/Scripts/GameMechanic/GyroscopeRotation.cs
+++ Assets/Mobile/Scripts/GameMechanic/GyroscopeRotation.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 
 public class GyroscopeRotation : MonoBehaviour
 {
-    private Quaternion _gyroInitialRotation;
     private Gyroscope _gyroscope;
     private bool _isGyroSupported;
     private Quaternion _lastRotation;
-    private Quaternion _offsetRotation;
+    private Quaternion _gyroInitialRotation;
 
 #if UNITY_EDITOR
     private void OnApplicationFocus(bool hasFocus)
@@ -22,20 +21,38 @@ public class GyroscopeRotation : MonoBehaviour
 
         _gyroscope = Input.gyro;
         _gyroscope.enabled = true;
-        Debug.Log(_gyroscope.enabled);
+        Debug.Log($"Gyroscope {(_gyroscope.enabled ? "enabled" : "disabled")}");
         Recalibrate();
     }
-    
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+
+    private void Recalibrate()
+    {
+        if (!_isGyroSupported)
+            return;
+
+        _gyroInitialRotation = GyroToUnity(_gyroscope.attitude);
+        print("Successfully recalibrated !");
+    }
+
+    private Quaternion AdjustGyro(Quaternion q)
+    {
+        q = Quaternion.Inverse(_gyroInitialRotation) * GyroToUnity(q);
+        (q.y, q.z) = (0f, q.y);
+        return q;
+    }
+
     private void FixedUpdate()
     {
         if (!_isGyroSupported)
             return;
-        
-        _offsetRotation = Quaternion.Inverse(_gyroInitialRotation) * GyroToUnity(Input.gyro.attitude);
-        _offsetRotation = new Quaternion(_offsetRotation.x, 0f, _offsetRotation.y, _offsetRotation.w);
 
-        GyroData data = new(_offsetRotation);
-        
+        GyroData data = new(AdjustGyro(_gyroscope.attitude));
+
         //Debug.Log($"{data.value} text {_lastRotation}");
         if (data.value == _lastRotation)
             return;
@@ -44,21 +61,5 @@ public class GyroscopeRotation : MonoBehaviour
         _lastRotation = data.value;
 
         //Debug.Log(data.value);
-    }
-
-
-    private Quaternion GyroToUnity(Quaternion gyro)
-    {
-        return new Quaternion(gyro.x, gyro.y, -gyro.z, -gyro.w);
-    }
-
-    public void Recalibrate()
-    {
-        Quaternion gyro = GyroToUnity(Input.gyro.attitude);
-        _gyroInitialRotation.x = gyro.x;
-        _gyroInitialRotation.y = gyro.y; // Fixed Y axis
-        _gyroInitialRotation.z = gyro.z; // We rotate object on Y with Z axis gyro
-        _gyroInitialRotation.w = gyro.w;
-        print("Successfully recalibrated !");
     }
 }
